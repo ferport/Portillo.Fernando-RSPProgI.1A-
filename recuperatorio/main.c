e1340650ca44d0ce22ade6df272d2d4c552090eb
ferport <portillofernandobek@gmail.com>
2020-11-30 10:01:56 -0300

    recuperatorio finalizado

---
+++ recuperatorio/main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    int id;
+    char marca[20];
+    int tipo;
+    float peso;
+}eVehiculo;
+
+eVehiculo* vehiculo_new();
+eVehiculo* vehiculo_newParametros(int id,char* marca,int tipo, float peso);
+int saveAsText(char* path , eVehiculo* vehiculo);
+int saveAsBinary(char* path , eVehiculo* vehiculo);
+void mostrarVehiculo(eVehiculo* vehiculo);
+
+int main()
+{
+    char marca[20] = {"Ford"};
+    eVehiculo* vehiculo = NULL;
+
+    vehiculo = vehiculo_newParametros(12,marca,12,1.5);
+    mostrarVehiculo(vehiculo);
+    /*vehiculo->id = 12;
+    vehiculo->peso = 1.5;
+    vehiculo->tipo = 13;
+    strcpy(vehiculo->marca, marca);*/
+
+    /*if(!saveAsText("data.cvs", vehiculo)){
+        printf("datos cargados correctamente\n");
+    }else{
+        printf("Error al cargar los datos");
+    }*/
+
+    if(!saveAsBinary("dataBin.bin", vehiculo)){
+        printf("datos cargados correctamente\n");
+    }else{
+        printf("Error al cargar los datos");
+    }
+
+    return 0;
+}
+
+eVehiculo* vehiculo_new(){
+    eVehiculo* nuevoVehiculo = NULL;
+    nuevoVehiculo = (eVehiculo*) malloc (sizeof(eVehiculo));
+
+    return nuevoVehiculo;
+}
+
+eVehiculo* vehiculo_newParametros(int id,char* marca,int tipo, float peso){
+    eVehiculo* nuevoVehiculo = vehiculo_new();
+
+    if(nuevoVehiculo != NULL){
+        nuevoVehiculo->id = id;
+        strcpy(nuevoVehiculo->marca, marca);
+        nuevoVehiculo->tipo = tipo;
+        nuevoVehiculo->peso = peso;
+    }
+    return nuevoVehiculo;
+}
+
+int saveAsText(char* path , eVehiculo* vehiculo)
+{
+    int error = 1;
+    int auxId;
+    float auxPeso;
+    int auxTipo;
+	char auxMarca[128];
+	FILE* pFile;
+
+	if(vehiculo != NULL && path != NULL){
+		pFile = fopen(path, "w");
+		auxId = vehiculo->id;
+		strcpy(auxMarca, vehiculo->marca);
+		auxTipo = vehiculo->tipo;
+		auxPeso = vehiculo->peso;
+		if(pFile != NULL){
+                fprintf(pFile, "%d,%s,%d,%f\n", auxId, auxMarca, auxTipo, auxPeso);
+                error = 0;
+            }
+			fclose(pFile);
+		}
+    return error;
+}
+
+int saveAsBinary(char* path , eVehiculo* vehiculo)
+{
+    int error = 1;
+	FILE* pFile;
+	eVehiculo* auxVehiculo = NULL;
+
+	if(vehiculo != NULL && path != NULL){
+		pFile = fopen(path, "wb");
+        auxVehiculo->id = vehiculo->id;
+		strcpy(auxVehiculo->marca, vehiculo->marca);
+		auxVehiculo->tipo = vehiculo->tipo;
+		auxVehiculo->peso = vehiculo->peso;
+        fwrite(auxVehiculo, sizeof(eVehiculo), 1, pFile);
+		fclose(pFile);
+		error = 0;
+	}
+    return error;
+}
+
+void mostrarVehiculo(eVehiculo* vehiculo){
+    if(vehiculo != NULL){
+        printf("  %04d %20s       %3d    %6f\n",vehiculo->id, vehiculo->marca, vehiculo->tipo, vehiculo->peso);
+    }
+}
